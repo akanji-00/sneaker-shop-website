bce4fb9fb825516cec5535b195a2fefccc654b3b
Mohammed Adetunji <theadetunji001@gmail.com>
2026-01-08 20:15:49 +0100

    Active changes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,22 @@
 // import React from "react";
+import { useState } from "react";
 import styles from "./Navigation.module.css";
 import Hamburger from "../../assets/images/icon-menu.svg";
 import Logo from "../../assets/images/logo.svg";
+import Close from "../../assets/images/icon-close.svg";
 import NavMenu from "./NavMenu";
 import NavIcons from "./NavIcons";
 
 function Navigation() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  function isOpen() {
+    if (toggleMenu) {
+      setToggleMenu(false);
+    } else setToggleMenu(true);
+    console.log("button clicked");
+  }
+
   return (
     <>
       <div className={styles.overlay}></div>
@@ -15,8 +26,9 @@ function Navigation() {
           <div className={styles.nav__left}>
             <button>
               <img
+                onClick={isOpen}
                 className={styles.hamburger}
-                src={Hamburger}
+                src={!toggleMenu ? Hamburger : Close}
                 alt="Hamburger menu"
               />
             </button>
